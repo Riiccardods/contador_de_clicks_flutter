91af469fd65aafc5951aff0b54c405c1ec576e12
riicardods <riicardoljn@icloud.com>
2024-07-22 12:20:06 -0300

    pasta teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:contador/screens/counter/counter_screen.dart';
+import 'package:contador/presentation/screens/counter/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
