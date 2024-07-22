1cf7c32da78a259a88e19fd2d9437a944221ce12
riicardods <riicardoljn@icloud.com>
2024-07-22 09:07:25 -0300

    Commit inicial

---
+++ lib/screens/counter/counter_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$clickCounter',
+                style: const TextStyle(
+                    fontSize: 190, fontWeight: FontWeight.w100)),
+
+            // Text('Click${clickCounter == 1 ? '':'s'}',
+
+            if (clickCounter == 1)
+              const Text('Click',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ))
+            else
+              const Text('Clicks',
+                  style: TextStyle(
+                    fontSize: 25,
+                  )),
+
+             Container(
+              height: 220.0,
+              color: Color.fromARGB(0, 248, 4, 4), // Espaço de 20 pixels de altura
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter = clickCounter + 1;
+          });
+        },
+        child: const Icon(Icons.plus_one),
+      ), // FloatingActionButton
+    );
+  }
+}
